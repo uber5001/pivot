d1f08ea8e59a6c3f30a4668caeabc4d96573d070
Scott Little <uber5001@stagebloc.com>
2013-10-12 07:22:15 -0400

    add networks

--- main.js
+++ main.js
@@ -14,16 +14,26 @@ function resize() {
 
 //run the game
 var gameTest = new PivotGame(1/*player*/, canvas.getContext('2d'));
-window.setInterval(function() {
-	var input = 0;
-	if (left) input++;
-	if (right) input--;
-	gameTest.update([input]);
-}, 1000 / 60);
+
 function render() {
 	gameTest.render();
 	requestAnimationFrame(render);
 } render();
+
+var gameTest = undefined;
+var ws = new WebSocket("ws://scottlittle.me:8080");
+ws.addEventListener('message', function(e) {
+	var msg = JSON.parse(e.data);
+	console.log(e.data);
+	if (msg.type == "tick") {
+		if (gameTest == undefined) {
+			console.log('foo');
+			gameTest = new PivotGame(msg.inputs.length, canvas.getContext('2d'));
+		}
+		gameTest.update(msg.inputs);
+	}
+});
+
 
 //TODO: don't hard-code it to be local!
 var left = false;
@@ -31,14 +41,27 @@ var right = false;
 window.addEventListener('keydown', function(e) {
 	if (e.keyCode == 37) {
 		left = true;
+		updateControls();
 	} else if (e.keyCode == 39) {
 		right = true;
+		updateControls();
 	}
 });
 window.addEventListener('keyup', function(e) {
 	if (e.keyCode == 37) {
 		left = false;
+		updateControls();
 	} else if (e.keyCode == 39) {
 		right = false;
+		updateControls();
 	}
 });
+function updateControls() {
+	var input = 0;
+	if (left) input++;
+	if (right)input--;
+	ws.send(JSON.stringify({
+		"type": "input",
+		"input": input
+	}));
+}
