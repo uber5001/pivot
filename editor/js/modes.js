46333a678822e3c956fced2d726aa2f7a3398376
Scott Little <uber5001@stagebloc.com>
2013-10-16 14:17:16 -0400

    editing experimental "modes"

--- editor/js/modes.js
+++ editor/js/modes.js
@@ -1,10 +1,12 @@
-var Mode = new function() {
-	this.onmousedown = function() {};
-	this.onmouseup = function() {};
-	this.onmousemove = function() {};
-	this.oncontextmenu = function() {};
-
-	this.ontouchdown = function() {};
-	this.ontouchup = function() {};
-	this.ontouchmove = function() {};
-}
+/**
+ * The purpose of a Mode is to gain input from
+ * the user about something. It's like giving
+ * the user a prompt box, but instead, the
+ * entire interface changes to allow the user
+ * to respond.
+ */
+var Mode = {};
+//polygon edit mode.
+//polgon edit mode (polygon has been selected)
+//goal edit mode
+//main mode (look at maap and choose mode)
--- editor/js/modes/polygon.js
+++ editor/js/modes/polygon.js
@@ -1,65 +1,44 @@
-Mode.Polygon = new function() {
-	this.onmousedown = function(e) {};
-	this.onmouseup = function(e) {};
-	this.onmousemove = function(e) {};
-	this.oncontextmenu = function(e) {};
-
-	this.ontouchdown = function(e) {};
-	this.ontouchup = function(e) {};
-	this.ontouchmove = function(e) {};
-
-	function ondown() {
+(function() {
+	Mode.Polygon = function(callback) {
+		addListeners();
+	}
+	function addListeners() {
+		window.addEventListener('mousedown',   onmousedown);
+		window.addEventListener('mouseup',     onmouseup);
+		window.addEventListener('mousemove',   onmousemove);
+		window.addEventListener('touchdown',   ontouchdown);
+		window.addEventListener('touchup',     ontouchup);
+		window.addEventListener('touchmove',   ontouchmove);
+		window.addEventListener('contextmenu', oncontextmenu);
+	}
+	function removeListeners() {
+		window.removeEventListener('mousedown',   onmousedown);
+		window.removeEventListener('mouseup',     onmouseup);
+		window.removeEventListener('mousemove',   onmousemove);
+		window.removeEventListener('touchdown',   ontouchdown);
+		window.removeEventListener('touchup',     ontouchup);
+		window.removeEventListener('touchmove',   ontouchmove);
+		window.removeEventListener('contextmenu', oncontextmenu);
+	}
+	function onmousedown() {
 
 	}
-	function onup() {
+	function onmouseup() {
 
 	}
-	function onmove() {
+	function onmousemove() {
 
 	}
-	function onhover() {
+	function ontouchdown() {
 
 	}
-	window.addEventListener('click', function(e) {
-		var ex = -x+(e.clientX-innerWidth/2)*z;
-		var ey = -y+(e.clientY-innerHeight/2)*z;
-		if (e.button === 0) {
-			var point = {"x":ex, "y":ey};
-			if (!pointInPolygon(point,currentPlatform)) {
-				if (currentPlatform.length > 0) {
-					if (currentPlatform.length > 1) {
-						var region = voronoi(point, currentPlatform);
-						while ((region.start+1)%currentPlatform.length != region.end) {
-							var removed = (region.start+1)%currentPlatform.length;
-							currentPlatform.splice((region.start+1)%currentPlatform.length, 1);
-							if (removed < region.end) region.end--;
-							if (removed < region.start) region.start--;
-						}
-						currentPlatform.splice((region.start+1)%currentPlatform.length,0,point);
-					} else {
-						currentPlatform.push(point);
-					}
-				} else {
-					//no platform being created.
-					for (var i = platforms.length-1; i >= 0; i--) {
-						if (pointInPolygon(point, platforms[i])) {
-							currentPlatform = platforms.splice(i,1)[0];
-							break;
-						}
-					}
-					if (currentPlatform.length === 0) {
-						currentPlatform.push(point);
-					}
-				}
-			} else {
-				//clicking inside currentPlatform
-				platforms.push(currentPlatform);
-				currentPlatform = [];
-			}
-		}
-		render();
-	});
-	window.addEventListener('contextmenu', function(e) {
+	function ontouchup() {
+
+	}
+	function ontouchmove() {
+
+	}
+	function oncontextmenu(e) {
 		var ex = -x+(e.clientX-innerWidth/2)*z;
 		var ey = -y+(e.clientY-innerHeight/2)*z;
 		if (pointInPolygon({x:ex,y:ey}, currentPlatform)) {
@@ -73,9 +52,8 @@ Mode.Polygon = new function() {
 			}
 		}
 		e.preventDefault();
-	});
-	*/
-	window.addEventListener('mousewheel', function(e) {
+	}
+	function onmousewheel(e) {
 		var ex = -x+(e.clientX-innerWidth/2)*z;
 		var ey = -y+(e.clientY-innerHeight/2)*z;
 		if (e.wheelDelta > 0) {
@@ -94,50 +72,7 @@ Mode.Polygon = new function() {
 			y += newy - ey;
 		}
 		render();
+	}
+	window.addEventListener('mousewheel', function(e) {
 	});
-
-
-
-	/*
-	window.addEventListener('keypress', function(e) {
-		if (e.keyCode === 13) {
-			//enter
-			platforms.push(currentPlatform);
-			currentPlatform = [];
-		}
-		render();
-	});
-	*/
-	var highlightedPlatform = null;
-	var highlightedPoint = -1;
-	/*
-	window.addEventListener('mousemove', function(e) {
-		var ex = -x+(e.clientX-innerWidth/2)*z;
-		var ey = -y+(e.clientY-innerHeight/2)*z;
-		var i;
-		highlightedPlatform = null;
-		highlightedPoint = -1;
-		if (currentPlatform.length > 0) {
-			for (var i = 0; i < currentPlatform.length; i++) {
-				var worldDist = Math.sqrt(Math.pow(ex - currentPlatform[i].x, 2) + Math.pow(ey - currentPlatform[i].y,2));
-				var screenDist = worldDist/z;
-				if (screenDist < 50) {
-					highlightedPoint = i;
-					break;
-				}
-			}
-			if (i == currentPlatform.length && pointInPolygon({x:ex,y:ey}, currentPlatform)) {
-				highlightedPlatform = currentPlatform;
-			}
-		} else  {
-			for (i = platforms.length-1; i >= 0; i--) {
-				if (pointInPolygon({x:ex,y:ey},platforms[i])) {
-					highlightedPlatform = platforms[i];
-					break;
-				}
-			}
-		}
-		render();
-	});
-	*/
 }
